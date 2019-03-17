0bd02096be034e81207ca70ca110b3cd0e7193f2
Rafael Azevedo <rafaelazevedogto@gmail.com>
2019-03-16 21:26:56 -0300

    Pag de criar task

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -8,9 +8,18 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['./create.page.scss'],
 })
 export class CreatePage implements OnInit {
-  constructor(private storage: Storage) {}
+  
+  subtasks: any[] = [" "];
+ 
+  constructor(private storage: Storage) {
+      
+  }
 
   ngOnInit() {
+  }
+
+  addSubTask(){
+    this.subtasks.push(" ");
   }
 
   create(title: string, items: any[]) {
